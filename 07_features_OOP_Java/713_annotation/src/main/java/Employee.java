7c921ef22113f71e30d07f921d6ced09f3e3b88e
raihem <rainium11@gmail.com>
2021-04-12 22:22:48 +0300

    713_maven_example

---
+++ 07_features_OOP_Java/713_annotation/src/main/java/Employee.java
@@ -0,0 +1,16 @@
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+
+    private String name;
+    private int salary;
+    private Date workStart;
+
+
+}
---
+++ 07_features_OOP_Java/713_annotation/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("Uji",2541, new Date());
+        System.out.println(employee);
+    }
+}
