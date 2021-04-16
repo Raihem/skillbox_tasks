8d085137809aac14ccf9a9e90a77eab543a23fef
raihem <rainium11@gmail.com>
2021-04-16 22:44:11 +0300

    mysql start

---
+++ 10_mysql/10_1_sql/JDBCExperimets/src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+
+public class Main {
+    public static void main(String[] args ) {
+       String url = "jdbc:mysql://localhost:3306/skillbox";
+       String user = "root";
+       String pass = "tetrartetrar";
+
+
+
+
+
+        try {
+            Connection connection = DriverManager.getConnection(url,user ,pass );
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM courses");
+            while (resultSet.next()){
+                String courseName = resultSet.getString("name");
+                System.out.println(courseName);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
