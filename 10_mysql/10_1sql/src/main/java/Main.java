54d2071e9d0526823d93e735b2600689c5e8e68c
raihem <rainium11@gmail.com>
2021-05-06 18:17:53 +0300

    sql_update

--- 10_mysql/10_1sql/src/main/java/Main.java
+++ 10_mysql/10_1sql/src/main/java/Main.java
@@ -2,24 +2,36 @@ import java.sql.Connection;
 import java.sql.*;
 
 public class Main {
+
+    private static final String sqlQuery = "SELECT course_name, " +
+            "COUNT(MONTH(subscription_date)) / COUNT(DISTINCT MONTH(subscription_date)) AS count_per_month " +
+            "FROM purchaselist GROUP BY course_name";
+
+
     public static void main(String[] args) {
 
         String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
         String pass = "tetrartetrar";
+        int purchase_count=0;
 
         try {
 
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM courses");
-            while (resultSet.next()){
-                String courseName = resultSet.getString("name");
-                System.out.println(courseName);
+//            Statement statement2 = connection.createStatement();
+
+//            обновляем название курса
+//            statement.execute("UPDATE courses SET name='Веб-разаботчик с zero до PRO' WHERE id=1");
+
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("course_name") + "\t" +
+                        resultSet.getString("count_per_month"));
             }
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+    } catch (NullPointerException | SQLException ex) {
+        ex.printStackTrace();
     }
+}
 }
