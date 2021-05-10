f11241ad37a85446e4c7450e066ee55989bf910e
raihem <rainium11@gmail.com>
2021-05-10 20:30:32 +0300

    Thread_test2

--- 11_multithread/11_test/src/Main.java
+++ 11_multithread/11_test/src/Main.java
@@ -1,44 +1,55 @@
 public class Main {
 
+// угадай число
+
+    static int ran =  (int)(Math.random() * 100);
+    static boolean isran = false;
+
+
     static boolean isFive = false;
     public static void main(String[] args) {
-        Thread timer = new Thread(new Runnable() {
-            @Override
-            public void run() {
+        System.out.println(ran+"\n");
 
-                try {
-                    int i = 0;
-                    while (true){
-                        if(i==5){
-                            isFive = true;
-                        }
-                        System.out.println(i);
-                        i++;
-                        Thread.sleep(1000);
-                    }
-                } catch (Exception e) {}
-                }
-
-        });
-        timer.start();
         Thread timer2 = new Thread(new Runnable() {
             @Override
             public void run() {
 
                 try {
-                    int i = 0;
-                    while (!isFive){
-                        if(i==5){
-                            isFive = true;
+
+                    while (true ){
+                        int i2 =  (int)(Math.random() * 100);
+                        if(i2==ran){
+                            isran=true;
+                            System.out.println("-> "+i2+" <-");
+                            break;
                         }
-                        System.out.println(i);
-                        i++;
-                        Thread.sleep(1000);
+                        System.out.println(i2);
+
+                        Thread.sleep(101);
+
                     }
                 } catch (Exception e) {}
             }
 
         });
         timer2.start();
+
+        Thread timer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    int i = 0;
+                    while (!isran ){
+
+                        System.out.println(i);
+                        i++;
+                        Thread.sleep(99);
+                    }
+                } catch (Exception e) {}
+            }
+
+        });
+        timer.start();
     }
 }
